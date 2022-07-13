cab158d9c0257e434b7d48684aa9f97b6652a776
Rednata <maknatosh@gmail.com>
2022-07-13 22:50:55 +0700

    compete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let s = [] ;
+  for (let i of bracketsConfig) {
+    s.push(i.join(''));  
+}
+
+let flag = true;
+while (flag) {
+  flag = false;
+  for (let i of s) {
+    let ind = str.indexOf(i);
+    if (ind>= 0) {      
+      str = str.slice(0,ind)+str.slice(ind+2);
+      flag = true;
+    }
+  }  
+}
+return (str.length===0) ? true : false;
 }
